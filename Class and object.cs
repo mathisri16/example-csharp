85a3785d84492c1e896876a4c39ba88a4a54b59c
mathisri16 <131431096+mathisri16@users.noreply.github.com>
2023-04-27 13:20:19 +0530

    Add files via upload

---
+++ Class and object.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp
+{
+    internal class Class_and_object
+    {
+        // A class is a blueprint that define the variables and the methods
+        //Common to all objects of a cretain kind
+
+        //object is an instance of a class
+
+        
+    }
+
+}
+
+class tree
+{
+    public string name;
+    public string family;
+    public int age;
+}
